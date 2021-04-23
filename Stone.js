78cac7d721ce7dced77700532c48eeec8029f007
mto2804 <80513586+mto2804@users.noreply.github.com>
2021-04-23 20:15:12 +1000

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -6,14 +6,13 @@ class Stone {
         'restitution':0.5
       };
       this.body = Bodies.rectangle(x, y, 70, 70, options);
-      this.width = 150;
-      this.height = 40;
+      this.width = 100;
+      this.height = 100;
       World.add(world, this.body);
     };
     display(){
       var pos = this.body.position;
-      pos.x = mouseX;
-      pos.y = mouseY;
+      
       var angle = this.body.angle;
   
       push();
